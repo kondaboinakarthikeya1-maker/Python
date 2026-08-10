2483b40840c04ebf1b91605e96b290027aa68be9
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-10 23:50:06 +0530

    to check largest of among of four numbers

---
+++ largest_of_four.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if a >= b and a >= c and a >= d:
+    print("a is greater")
+elif b >= a and b >= c and b >= d:
+    print("b is greater")
+elif c >= a and c >= b and c >= d:
+    print("c is greater")
+else:
+    print("d is greater")
