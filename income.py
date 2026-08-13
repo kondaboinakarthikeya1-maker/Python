2700888df9b3fb93c17c11968be098a539215999
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-13 22:09:52 +0530

    TO check income_tax slab based on their income

---
+++ income.py
@@ -0,0 +1,8 @@
+income = float(input("Enter your income : "))
+if income <= 250000:
+    tax = 0
+elif income <= 500000:
+    tax = income * 0.05
+elif income <= 1000000:
+    tax = income * 0.20
+print(tax)
