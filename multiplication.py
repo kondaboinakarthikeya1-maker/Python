1f568fc23ee1dd8d63848aac84e8b0fac59c843d
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-19 23:48:02 +0530

    To print multiplication table of a number

---
+++ multiplication.py
@@ -0,0 +1,4 @@
+n = int(input("Enter a number : "))
+for i in range(1,11):
+    s = n * i
+    print(n,"X",i ,"=", s)
--- reverse_even.py
+++ reverse_even.py
@@ -1,7 +1,7 @@
 n = int(input("Enter a number : "))
-if n % 3 == 0:
-    start = n 
+if n % 2 == 0:
+    start = n
 else:
-    start = n - (n % 3)
-for i in range(start,0,-3):
+    start = n-1
+for i in range(start,0,-2):
     print(i)
