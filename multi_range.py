05aece19b182c2cb791888d8914dfcb2ca4bdaf1
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-19 23:55:05 +0530

    TO print multiplication of a table in a range

---
+++ multi_range.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number : "))
+for n in range(1,6):
+    for i in range(1,11):
+        s = n * i
+        print(n,"X",i ,"=", s)
--- multiplication.py
+++ multiplication.py
@@ -1,4 +1,5 @@
-n = int(input("Enter a number : "))
-for i in range(1,11):
-    s = n * i
-    print(n,"X",i ,"=", s)
+n = int(input("Enter a number to print table upto : "))
+for i in range(1,n+1):
+    for i in range(1,11):
+        s = n * i
+        print(n,"X",i ,"=", s)
