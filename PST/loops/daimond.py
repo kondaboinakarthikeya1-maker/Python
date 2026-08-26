dff776328637197e05fb989cf64650bc61010043
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-26 23:27:46 +0530

    To print daimond with stars

---
+++ PST/loops/daimond.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number : "))
+for i in range(1,n+1):
+    space = n - i
+    star = 2 * i - 1
+    print(" " * space + "*" * star)
+for i in range(n-1,0,-1):
+    space = n - i
+    star = 2 * i - 1
+    print(" " * space + "*" * star)
