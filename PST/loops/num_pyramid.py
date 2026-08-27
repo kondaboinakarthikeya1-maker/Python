9b39529ca55a198bc8eb53acdcd77daa00f1a957
Kondaboina karthikeya <kondaboinakarthikeya1@gmail.com>
2026-08-27 22:53:13 +0530

    To print pyramid of a reversed numbers

---
+++ PST/loops/num_pyramid.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number : "))
+for i in range(1,n+1):
+    space = n - i
+    print(" " * space,end = "")
+    for j in range(1,i + 1):
+        print(j,end = "")
+    for j in range(i - 1,0,-1):
+            print(j,end = "")
+    print()
